ed9ae1986b01a7b9dab706a55d3d27da14466e4a
™NixonHype® <92142624+HYPER-AD17@users.noreply.github.com>
2022-03-25 11:46:07 +0530

    Hyper

--- lunaBot/modules/__nightmode.py
+++ lunaBot/modules/__nightmode.py
@@ -129,7 +129,7 @@ async def job_close():
     for pro in chats:
         try:
             await tbot.send_message(
-              int(pro.chat_id), "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By luna**"
+              int(pro.chat_id), "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By Miss-Sofia🧚‍♀️**"
             )
             await tbot(
             functions.messages.EditChatDefaultBannedRightsRequest(
@@ -151,7 +151,7 @@ async def job_open():
     for pro in chats:
         try:
             await tbot.send_message(
-              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered By luna**"
+              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered By Miss-Sofia🧚‍♀️**"
             )
             await tbot(
             functions.messages.EditChatDefaultBannedRightsRequest(
