ad369e22b1376fadca5b21de88ce164f88575dd9
™NixonHype® <92142624+HYPER-AD17@users.noreply.github.com>
2022-03-25 12:23:35 +0530

    Update mhelp.py

--- lunaBot/modules/mhelp.py
+++ lunaBot/modules/mhelp.py
@@ -1,17 +1,5 @@
-import re
-import os
-
-from telethon import events, Button
-from telethon import __version__ as tlhver
-from pyrogram import __version__ as pyrover
-from lunaBot.events import register as MEMEK
-from lunaBot import telethn as tbot
-
-PHOTO = "https://telegra.ph/file/7c3c26e0ed938aec91209.jpg"
-
-@MEMEK(pattern=("/mhelp"))
-async def awake(event):
-  tai = event.sender.first_name
+"""
+tai = event.sender.first_name
   LUNA = "** ──「 Basic Guide 」── ** \n\n"
   LUNA += "• /play **(song title) — To Play the song you requested via YouTube** \n"
   LUNA += "• /search ** (song/video title) – To search for links on YouTube with details** \n"
@@ -24,6 +12,72 @@ async def awake(event):
   LUNA += "• /end - **To Stop Song playback** \n"
   LUNA += "• /control - **open the player settings panel** \n"
   LUNA += "• /reload - **To Refresh admin list** \n"
+"""
 
-  BUTTON = [[Button.url("☎️ Support", "https://t.me/lunaXresso"), Button.url("📡 Updates", "https://t.me/ZeinzoProject")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
+import random
+import re
+import os
+import requests
+
+from telegram import (
+    ChatPermissions,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ParseMode,
+    Update,
+)
+from typing import List
+from telegram.ext import CallbackContext, run_async
+from telethon import events, Button
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from lunaBot.events import register as MEMEK
+from lunaBot import telethn as tbot
+from lunaBot.modules.disable import DisableAbleCommandHandler
+
+#PHOTO = "https://telegra.ph/file/7c3c26e0ed938aec91209.jpg"
+
+MEME_STICKER = [
+    "CAACAgUAAx0CYt4kYwACKp5iPWIPs1bTz845pTA-QyuPogUnfgACdQQAAoTOcFUe8m3_76viiiME",
+    "CAACAgUAAx0CYt4kYwACKp1iPWIOop1W3R64NdUMmc8xzl2FXQACNQQAAk84aVVNKEfsfquoOSME",
+    "CAACAgUAAx0CYt4kYwACKpdiPWIHZsUMtqBZHgEsJ-Vk3aeygQACGgMAAgEEWVUnEJR1saZrIyME",
+    "CAACAgUAAx0CYt4kYwACKpZiPWIGQs8dsfQjQnkNXC8LV8QhjwACJwUAArDGWFU7Xr2JJzFXpSME",
+    "CAACAgUAAx0CYt4kYwACKpViPWIFZV0zqoVAJQrCOe9AHBedoQACZwUAAlg_WFWLiAi3FnLHXSME",
+    "CAACAgUAAx0CYt4kYwACKpRiPWIBDC5iPUKxP4apeAwBJ0XHGwAC_wIAAhNHWFVp6A_D2GnrhCME",
+    "CAACAgUAAx0CYt4kYwACKpNiPWH_BmPUHqiW4zlMC9Xt5ieaQwACYgMAAiVKWFWbkw0u4Eac_yME",
+    "CAACAgIAAx0CYt4kYwACKrBiPWL7xiYUWcbfT_oVr5Vg-u7ZzwACeBUAAq6O4UlqYf9FNzvvzSME",
+    "CAACAgIAAx0CYt4kYwACKq1iPWLr2mKktso1oAzvbJkglshBPAACsBQAAvgD6UlWlIfaVWuw6yME",
+]
+
+@run_async
+def xnxx(update: Update, context: CallbackContext):
+    #msg = update.effective_message
+    pingpong = random.choice(LETS_GO_OYO)
+
+    update.effective_message.reply_sticker(
+        MEME_STICKER, reply_markup=InlineKeyboardMarkup(
+                            [
+                                {
+                                    InlineKeyboardButton(
+                                        text="ᴄʀᴇᴀᴛᴏʀ🧚‍♀️",
+                                        url="tg://user?id=5072650671"),
+                                }
+                            ]
+                        ),
+        parse_mode=ParseMode.MARKDOWN,
+    )
+
+MEME_HANDLER = DisableAbleCommandHandler("dc", xnxx)
+
+
+dispatcher.add_handler(MEME_HANDLER)
+
+__command_list__ = ["dc"]
+
+__handlers__ = [MEME_HANDLER]
+
+__mod_name__ = "Xtra-Fun"
+
+__help__ = """Extra Funn Command (only for fun)\n\n✨ /dc : Use nd see. \n\n More command soon keep patients!!"""
+
+
